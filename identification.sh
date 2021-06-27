11f4744ea3a65af6018e7076e66962a0211d4588
ys-lim <68455070+ys-lim@users.noreply.github.com>
2021-06-27 22:03:44 +0800

    Update identification.sh

--- identification.sh
+++ identification.sh
@@ -23,6 +23,7 @@ grep exonic_part Mus_musculus.GRCm38.94.dexseq.gtf > Mus_musculus.GRCm38.94.dexs
 ############################
 # samtools view -F 256 should keep out secondary giving primary aligned only
 # awk gets the spliced reads "N" in cigar
+## PURPOSE: Extract spliced reads from bam alignment files
 samtools view -h -F 256 /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr1/ctr1.bam | awk '$1~/@/ || $6~/N/' | samtools view -bh > ctr/ctr1_spliced_reads.bam
 
 samtools view -h -F 256 /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr2/ctr2.bam | awk '$1~/@/ || $6~/N/' | samtools view -bh > ctr/ctr2_spliced_reads.bam
@@ -35,6 +36,7 @@ samtools view -h -F 256 /mnt/gtklab01/ira/tdp43_cnp_scn/star/cKO2/cKO2.bam | awk
 
 samtools view -h -F 256 /mnt/gtklab01/ira/tdp43_cnp_scn/star/cKO3/cko3.bam | awk '$1~/@/ || $6~/N/' | samtools view -bh > cko/cko3_spliced_reads.bam
 
+## PURPOSE: Intersect spliced reads with exon gtf file
 bedtools intersect -a ctr1_spliced_reads.bam -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf > ctr1_spliced_exons.bam
 
 bedtools intersect -a ctr2_spliced_reads.bam -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf > ctr2_spliced_exons.bam
@@ -47,6 +49,7 @@ bedtools intersect -a cko2_spliced_reads.bam -b ../../Mus_musculus.GRCm38.94.dex
 
 bedtools intersect -a cko3_spliced_reads.bam -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf > cko3_spliced_exons.bam
 
+## PURPOSE: Convert bam to bed file
 bedtools bamtobed -i ctr1_spliced_exons.bam -split | sort -k1,1 -k2,2n > ctr1_spliced_exons.bed
 
 bedtools bamtobed -i ctr2_spliced_exons.bam -split | sort -k1,1 -k2,2n > ctr2_spliced_exons.bed
@@ -59,6 +62,7 @@ bedtools bamtobed -i cko2_spliced_exons.bam -split | sort -k1,1 -k2,2n > cko2_sp
 
 bedtools bamtobed -i cko3_spliced_exons.bam -split | sort -k1,1 -k2,2n > cko3_spliced_exons.bed
 
+## PURPOSE: Inverse intersect spliced exons bed with same exon gff file
 bedtools intersect -a ctr1_spliced_exons.bed -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf -v > ctr1_spliced_introns.bed
 
 bedtools intersect -a ctr2_spliced_exons.bed -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf -v > ctr2_spliced_introns.bed
@@ -71,29 +75,42 @@ bedtools intersect -a cko2_spliced_exons.bed -b ../../Mus_musculus.GRCm38.94.dex
 
 bedtools intersect -a cko3_spliced_exons.bed -b ../../Mus_musculus.GRCm38.94.dexseq.exons.only.gtf -v > cko3_spliced_introns.bed
 
+#################################
+## PART 2: MERGING & GFF CREATION
+#################################
 
+## PURPOSE: Group all intronic mapping reads bed together (ko and control)
 cat cko1_spliced_introns.bed cko2_spliced_introns.bed cko3_spliced_introns.bed ../ctr/ctr1_spliced_introns.bed ../ctr/ctr2_spliced_introns.bed ../ctr/ctr3_spliced_introns.bed | sort -S 50% -k1,1 -k2,2n > grouped_spliced_introns_sorted.bed
 
+## PURPOSE: Merge spliced introns within 500bp of each other
 bedtools merge -i grouped_spliced_introns_sorted.bed -d 500 -c 1 -o count > spliced_introns_merged.bed
 
+## PURPOSE: Intersect merged spliced introns file with introns BED file 
 bedtools intersect -a spliced_introns_merged.bed -b ../Mus_musculus.GRCm38.94.dexseq.introns.only.bed -wb | awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4,$8,$9}' | sort -k1,1V -k5,5n > cryptics_merged.bed
 
+## PURPOSE: Filter unique cryptic tags
 cat cryptics_merged.bed | awk '{print $6}' | sort -V | uniq > unique_gene_introns.tab
 
+## PURPOSE: create .sh script to create unique list of cryptic exons annotated with unique id
 N=8
 for entry in `cat unique_gene_introns.tab`; do
 ((i=i%N)); ((i++==0)) && wait # this line prevents overloading for no. of running forks
 grep $entry cryptics_merged.bed | awk 'BEGIN{s=1}{print $0"i"s;s+=1}' >> cryptics_merged_annotated.bed
 done
 
+## PURPOSE: Convert cryptic exon bed file into gff file for merging
 cat cryptics_merged_annotated.bed | awk 'BEGIN{OFS="\t"}{split($6,a,"_");print $1, "Mus_musculus.GRCm38.94.gtf", "exonic_part", $2, $3, ".", $5, ".", "transcripts \"cryptic_exon\"; exonic_part_number \""a[2]"\"; gene_id \""a[1]"\"" }' | sort -k1,1 -k2,2n > cryptic_exons.gff
 
-
+## PURPOSE: Concatenate cryptic exons within total exon gff & sort - all_exons_and_cryptics.gff forms the annotation for subsequent cryptic tag hunting
 cat cryptic_exons.gff Mus_musculus.GRCm38.94.dexseq.gtf | sort -k1,1V -k4,4n -k5,5n | awk '$14 ~ /ENS/' > all_exons_and_cryptics.gff
+
+#########################
+## PART 3: READ COUNTING
+#########################
 
 samtools sort -n /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr1/ctr1.bam -o ctr1_sorted.bam
 
-samtools sort -n /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr1/ctr2.bam -o ctr2_sorted.bam #11.37am-12.21pm
+samtools sort -n /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr1/ctr2.bam -o ctr2_sorted.bam 
 
 samtools sort -n /mnt/gtklab01/ira/tdp43_cnp_scn/star/ctr1/ctr3.bam -o ctr3_sorted.bam
 
@@ -103,11 +120,10 @@ samtools sort /mnt/gtklab01/ira/tdp43_cnp_scn/star/cKO3/cko2.bam -o cko2_sorted.
 
 samtools sort /mnt/gtklab01/ira/tdp43_cnp_scn/star/cKO3/cko3.bam -o cko3_sorted.bam
 
-## read counting using star files --> no missing mate warning! 
 
-# read counting for scn samples (star) using actual all exons and cryptics gtf file
+# read counting for scn samples (star) using actual all_exons_and_cryptics.gff 
 python dexseq_count.py -s reverse -p yes -r name -f bam all_exons_and_cryptics.gff ctr/ctr1_sorted.bam ctr/ctr1_pycount.txt &> ctr/ctr1_pycount_report.txt &
-#11.37am-1pm
+
 python dexseq_count.py -s reverse -p yes -r name -f bam all_exons_and_cryptics.gff ctr/ctr2_sorted.bam ctr/ctr2_pycount.txt &> ctr/ctr2_pycount_report.txt &
 
 python dexseq_count.py -s reverse -p yes -r name -f bam all_exons_and_cryptics.gff ctr/ctr3_sorted.bam ctr/ctr3_pycount.txt &> ctr/ctr3_pycount_report.txt &
